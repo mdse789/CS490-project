67d89bb0f39338d31c0337dacbd72ec680efcae4
Litzy Reyes <litzy@MBP.local>
2026-02-06 15:57:51 -0500

    Connected Flask to MySQL using SQLAlchemy

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,23 @@
+import os
 from flask import Flask, jsonify
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+
+load_dotenv() 
 
 app = Flask(__name__)
 CORS(app)   # allow requests from React
+
+user = os.getenv('DB_USER')
+password = os.getenv('DB_PASSWORD')
+host = os.getenv('DB_HOST')
+database = os.getenv('DB_NAME')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+mysqlconnector://{user}:{password}@{host}/{database}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 @app.route("/")
 def home():
