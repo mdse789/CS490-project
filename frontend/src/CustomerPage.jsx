6160060c6b98ba3d64c0d95c58e4c5c39f85b1a5
mdse789 <md789@njit.edu>
2026-02-17 13:21:55 -0500

    changes that I forgot to push

--- frontend/src/CustomerPage.jsx
+++ frontend/src/CustomerPage.jsx
@@ -1,37 +1,70 @@
 /* List all customer using pagination*/
 import { useEffect, useState } from "react";
+import Modal from "./ModalPage";
 
 function CustomerPage({ onBack }) {
-
+  const [searchTerm, setSearchTerm] = useState("");
   const [customers, setCustomers] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(0);
-  const fetchCustomers = (page) => {
-    fetch(`http://127.0.0.1:5000/api/customer_all?page=${page}`)
+
+  const [selectedCustomer, setSelectedCustomer] = useState(null);
+
+  const fetchCustomers = (page, query = "") => {
+    fetch(`http://127.0.0.1:5000/api/customer_all?page=${page}&search=${query}`)
       .then((res) => res.json())
       .then((data) => {
         setCustomers(data.customers);
         setTotalPages(data.total_pages);
       })
       .catch((err) => console.error("Error fetching customer data", err));
-  };
+};
 
   useEffect(() => {
-    fetchCustomers(currentPage);
+    fetchCustomers(currentPage, searchTerm);
   }, [currentPage]);
 
+  const handleSearch = () => {
+    setCurrentPage(1);
+    fetchCustomers(1, searchTerm);
+};
+
+const handleCardClick = (customer) => {
+  
+    fetch(`http://127.0.0.1:5000/api/customer_details/${customer.id}`)
+      .then(res => res.json())
+      .then(fullData => {
+        setSelectedCustomer(fullData);
+      })
+      .catch(err => console.error("Error fetching details:", err));
+  };
  
 return (
  <div>
 <div className="customer-page-container">
       <button onClick={onBack}>Back to Home</button>
       <h1>Customer Page</h1>
+      
+      <h2>Customer Search</h2>
+      <p1>Enter Name or Id</p1>
+   <div className="search-bar">
+  <input
+    type="text"
+    placeholder="Search name or ID..."
+    value={searchTerm}
+    onChange={(e) => setSearchTerm(e.target.value)}
+  />
+  <button onClick={handleSearch}>Search</button>
+</div>
 
       <div className="results-list">
         {customers.map((customer) => (
-          <div key={customer.id} className="customer-card">
-            <h2>{"Id: "}{customer.id}</h2>
-            <h3>{"Name: "}{customer.first_name} {customer.last_name}</h3>
+          <div key={customer.id} className="customer-card" 
+          onClick={() =>handleCardClick(customer)}
+          style={{cursor: 'help'}}
+          >
+            <h3>{"Id: "}{customer.id}</h3>
+            <h4>{"Name: "}{customer.first_name} {customer.last_name}</h4>
             <p>{"Email: "}{customer.email}</p>
           </div>
         ))}
@@ -45,9 +78,7 @@ return (
         >
           Previous
         </button>
-
         <span> Page {currentPage} of {totalPages} </span>
-
         <button 
           disabled={currentPage === totalPages} 
           onClick={() => setCurrentPage(prev => prev + 1)}>
@@ -55,6 +86,21 @@ return (
         </button>
       </div>
       
+      <Modal 
+          open={selectedCustomer !== null} 
+          onClose={() => setSelectedCustomer(null)}
+      >
+          {selectedCustomer && (
+                <div className="modal-inner-content">
+                  <strong> Id:</strong> {selectedCustomer.id}
+                  <h2>{selectedCustomer.first_name} {selectedCustomer.last_name}</h2> 
+                  <p>Email:  {selectedCustomer.email} </p>
+                  <p>Address: {selectedCustomer.address}</p>
+                  <p>Phone: {selectedCustomer.phone}</p>
+              </div>
+            )}
+          </Modal>
+
  </div>
   );
 }
--- frontend/src/FilmsPage.jsx
+++ frontend/src/FilmsPage.jsx
@@ -1,8 +1,12 @@
 import { useEffect, useState } from "react";
+import Modal from "./ModalPage";
 
 function FilmsPage({ onBack }) {
   const [searchTerm, setSearchTerm] = useState("");
   const [films, setFilms] = useState([]);
+
+  const [selectedFilm, setSelectedFilm] = useState(null);
+  
 
   const fetchFilms = (query = "") => {
       fetch(`http://127.0.0.1:5000/api/films?search=${query}`)
@@ -11,14 +15,20 @@ function FilmsPage({ onBack }) {
       .catch((err) => console.error("Error fetching films:", err));
   };
 
+ const handleCardClick = (film) => {
+  fetch(`http://127.0.0.1:5000/api/film_details/${film.id}`)
+    .then(res => res.json())
+    .then(fullData => {
+      setSelectedFilm(fullData); 
+      setIsOpen(true);          
+    });
+};
+
   return (
  <div>
 
   <button onClick={onBack}>Back to Home</button>
-
-
-
-<div className="films-page-container">
+  <div className="films-page-container">
       
       <h1>Film Search</h1>
       <p1>Enter Film Name, Actor Name or Genre</p1>
@@ -31,22 +41,40 @@ function FilmsPage({ onBack }) {
         />
         <button onClick={() => fetchFilms(searchTerm)}>Search</button>
       </div>
-
+    
       <div className="results-list">
         {films.map((film) => (
-          <div key={film.id} className="film-card">
+          <div key={film.id} className="film-card"
+            onClick={() => handleCardClick(film)}
+            style={{ cursor: 'help'}}
+          >
             <h2>{"Id: "} {film.id}</h2>
             <h3>{film.title} ({film.year})</h3>
-            <p>{film.description}</p>
           </div>
         ))}
       </div>
+      
+      
+  <Modal 
+      open={selectedFilm !== null} 
+      onClose={() => setSelectedFilm(null)}
+  >
+      {selectedFilm && (
+        <div className="modal-inner-content">
+          <h2>{selectedFilm.title}</h2> <strong> Film Id:</strong> {selectedFilm.id}
+          <div> <strong>Release Year:</strong> {selectedFilm.year} </div>
+          <p>{selectedFilm.description}</p>
+          <p>Length: {selectedFilm.length} min</p>
+          <p>Rating: {selectedFilm.rating}</p>
+      </div>
+    )}
+  </Modal>
+
     </div>
       
  </div>
   );
 }
-
 
 
 
