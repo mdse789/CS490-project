0f897e813cebdcd9a962875531696ab22b7d415c
Litzy Reyes <litzy@MBP.local>
2026-02-17 02:08:39 -0500

    Updated landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,29 +11,29 @@ function Landingpage({ onNavigate }) {
   const [selectedItem, setSelectedItem] = useState(null);
   const handleOpenModal = (item) => {
     if (item.film_id) {
-    fetch(`http://127.0.0.1:5000/api/film_details/${item.film_id}`)
-      .then(res => res.json())
-      .then(data => {
-        setSelectedItem(data); 
-        setIsOpen(true);
-      })
-      .catch(err => console.error("Error fetching film details:", err));
-  } else if (item.actor_id) {
-    fetch(`http://127.0.0.1:5000/api/actors/${item.actor_id}`)
-      .then(res => res.json())
-      .then(data => {
-        setSelectedItem(data); 
-        setIsOpen(true);
-      });
-  }
-};
-  
+      fetch(`http://127.0.0.1:5000/api/film_details/${item.film_id}`)
+        .then(res => res.json())
+        .then(data => {
+          setSelectedItem(data);
+          setIsOpen(true);
+        })
+        .catch(err => console.error("Error fetching film details:", err));
+    } else if (item.actor_id) {
+      fetch(`http://127.0.0.1:5000/api/actors/${item.actor_id}`)
+        .then(res => res.json())
+        .then(data => {
+          setSelectedItem(data);
+          setIsOpen(true);
+        });
+    }
+  };
+
   useEffect(() => {
     fetch("http://127.0.0.1:5000/api/top5films")
       .then(res => res.json())
       .then(data => setTopFilms(data))
-      
-      
+
+
 
     fetch("http://127.0.0.1:5000/api/top5actors")
       .then(res => res.json())
@@ -41,20 +41,22 @@ function Landingpage({ onNavigate }) {
   }, []);
   return (
     <div className="landing-container">
-        <h1>Welcome to the Rental Store</h1>
+      <div className="title-block">
+      <h1>Welcome to the Rental Store</h1>
 
-    <div>
-    <button onClick={() => onNavigate("films")}>Films</button>
-    <button onClick={() => onNavigate("customer")}>Customer Page</button>
-    </div>
+      <div>
+        <button onClick={() => onNavigate("films")}>Films</button>
+        <button onClick={() => onNavigate("customer")}>Customer Page</button>
+      </div>
+      </div>
 
-    <div className="tables-grid">
-      <div className="tables-wrapper">
-        <h2>Top 5 Rented Films</h2>
-        <table>
+      <div className="tables-grid">
+        <div className="tables-wrapper">
+          <h2>Top 5 Rented Films</h2>
+          <table>
             <thead>
               <tr>
-                <th>Film ID</th>            
+                <th>Film ID</th>
                 <th>Title</th>
                 <th>Category</th>
                 <th>Rentals</th>
@@ -62,10 +64,10 @@ function Landingpage({ onNavigate }) {
             </thead>
             <tbody>
               {topFilm.map((film) => (
-                <tr 
-                key={film.film_id}
-                onClick={() => handleOpenModal(film)}
-                className="click-row"
+                <tr
+                  key={film.film_id}
+                  onClick={() => handleOpenModal(film)}
+                  className="click-row"
                 >
                   <td>{film.film_id}</td>
                   <td>{film.title}</td>
@@ -75,8 +77,8 @@ function Landingpage({ onNavigate }) {
               ))}
             </tbody>
           </table>
-      </div>
-      <div className="tables-wrapper">
+        </div>
+        <div className="tables-wrapper">
           <h2>Top 5 Actors</h2>
           <table>
             <thead>
@@ -89,8 +91,8 @@ function Landingpage({ onNavigate }) {
             <tbody>
               {topActors.map((actor) => (
                 <tr key={actor.actor_id}
-                onClick={() => handleOpenModal(actor)}
-                className="click-row"
+                  onClick={() => handleOpenModal(actor)}
+                  className="click-row"
                 >
                   <td>{actor.actor_id}</td>
                   <td>{actor.first_name} {actor.last_name}</td>
@@ -102,48 +104,46 @@ function Landingpage({ onNavigate }) {
         </div>
       </div>
       <Modal open={isOpen} onClose={() => setIsOpen(false)}>
-      {selectedItem && (
-        <div className="modal-content">
-          {selectedItem.title ? (
-          <>
-            <h2>{selectedItem.title}</h2>
-            <p><strong>Film ID:</strong> {selectedItem.id}</p>
-            <p><strong>Description:</strong> {selectedItem.description}</p>
-            <p><strong>Release Year:</strong> {selectedItem.year}</p>
-            <p><strong>Rating:</strong> {selectedItem.rating}</p>
-            <p><strong>Length:</strong> {selectedItem.length}</p>
-            <p><strong>Replacement Cost:</strong> {selectedItem.replacement_cost}</p>
-            <p><strong>Special Features:</strong> {selectedItem.special_features}</p>
-          </>
-        ) : (
-          <div className="actor-details">
-    <h3>Top 5 Movies</h3>
-    {Array.isArray(selectedItem) ? (
-      selectedItem.map((film) => (
-        <div key={film.film_id} className="actor-film-row">
-          <p><strong>Film ID:</strong> {film.film_id}</p>
-          <p><strong>Title:</strong> {film.title}</p>
-          <hr />
-        </div>
-      ))
-    ) : (
-      <p>No film data found for this actor.</p>
-    )}
-  </div>
-
-      )}
-    </div>
-    )}
-
-    </Modal>
+        {selectedItem && (
+          <div className="modal-content">
+            {selectedItem.title ? (
+              <>
+                <h2>{selectedItem.title}</h2>
+                <p><strong>Film ID:</strong> {selectedItem.id}</p>
+                <p><strong>Description:</strong> {selectedItem.description}</p>
+                <p><strong>Release Year:</strong> {selectedItem.year}</p>
+                <p><strong>Rating:</strong> {selectedItem.rating}</p>
+                <p><strong>Length:</strong> {selectedItem.length}</p>
+                <p><strong>Replacement Cost:</strong> {selectedItem.replacement_cost}</p>
+                <p><strong>Special Features:</strong> {selectedItem.special_features}</p>
+              </>
+            ) : (
+              <div className="actor-details">
+                <h3>Top 5 Movies</h3>
+                {Array.isArray(selectedItem) ? (
+                  selectedItem.map((film) => (
+                    <div key={film.film_id} className="actor-film-row">
+                      <p><strong>Film ID:</strong> {film.film_id}</p>
+                      <p><strong>Title:</strong> {film.title}</p>
+                      <hr />
+                    </div>
+                  ))
+                ) : (
+                  <p>No film data found</p>
+                )}
+              </div>
+            )}
+          </div>
+        )}
+      </Modal>
     </div>
   );
-  
+
 }
 
 function App() {
   const [view, setView] = useState("home");
-  
+
   useEffect(() => {
     fetch("http://localhost:5000/")
       .then(res => res.json())
@@ -151,13 +151,13 @@ function App() {
       .catch(err => console.error(err));
   }, []);
 
- return (
+  return (
     <div>
-     <div>
-      {view === "home" && <Landingpage onNavigate={setView} />}
-      {view === "films" && <FilmsPage onBack={() => setView("home")} />}
-      {view === "customer" && <CustomerPage onBack={() => setView("home")} />}  
-    </div>
+      <div>
+        {view === "home" && <Landingpage onNavigate={setView} />}
+        {view === "films" && <FilmsPage onBack={() => setView("home")} />}
+        {view === "customer" && <CustomerPage onBack={() => setView("home")} />}
+      </div>
     </div>
   );
 }
